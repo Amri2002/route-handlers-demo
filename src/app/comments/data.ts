006a9f6fc413902dd2a157c24664ec87e9941192
Amri <muhammedamri2002@gmail.com>
2025-02-22 13:34:14 +0530

    Handling GET Request

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id: 1,
+        comment: 'This is the irst comment',
+    },
+    {
+        id: 2,
+        comment: 'This is the second comment',
+    },
+    {
+        id: 3,
+        comment: 'This is the third comment',
+    },
+    ];
