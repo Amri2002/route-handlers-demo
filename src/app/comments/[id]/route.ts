78aa0d8fbab2d8a88b74ffa2389ce0215ea15dc5
Amri <muhammedamri2002@gmail.com>
2025-03-11 22:28:38 +0530

    changes for next15

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,34 +1,34 @@
 import { comments } from "../data";
 
-export async function GET(request: Request,
-    {params}: {params: {id: string}}
-){
-    const comment1 = comments.find(
-        (comment2) => comment2.id === parseInt(params.id));
-    return Response.json(comment1);
+export async function GET(
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const comment = comments.find((comment) => comment.id === parseInt(id));
+  return Response.json(comment);
 }
 
 export async function PATCH(
-    request: Request,
-    {params}: {params: {id: string}}
-){
-    const body = await request.json();
-    const {text} = body;
-    const index = comments.findIndex(
-        (comment987) => comment987.id === parseInt(params.id)
-    );
-    comments[index].comment = text;
-    return Response.json(comments[index]);
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  comments[index].text = text;
+  return Response.json(comments[index]);
 }
 
 export async function DELETE(
-    request: Request,
-    {params}: {params: {id: string}}
-){
-    const index = comments.findIndex(
-        (comment123) => comment123.id === parseInt(params.id)
-    );
-    const deletedComment = comments[index];
-    comments.splice(index, 1);
-    return Response.json(deletedComment);
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  const deletedComment = comments[index];
+  comments.splice(index, 1);
+  return Response.json(deletedComment);
 }
--- src/app/comments/data.ts
+++ src/app/comments/data.ts
@@ -1,14 +1,14 @@
 export const comments = [
-    {
-        id: 1,
-        comment: 'This is the irst comment',
-    },
-    {
-        id: 2,
-        comment: 'This is the second comment',
-    },
-    {
-        id: 3,
-        comment: 'This is the third comment',
-    },
-    ];
+  {
+    id: 1,
+    text: "This is the irst comment",
+  },
+  {
+    id: 2,
+    text: "This is the second comment",
+  },
+  {
+    id: 3,
+    text: "This is the third comment",
+  },
+];
